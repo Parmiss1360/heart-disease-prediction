7e47a82977a220d671acdd332b94a331807dc9ee
Parmis etesadpoor <parmisse@gmail.com>
2026-04-18 19:55:39 +0200

    ..

--- src/main.py
+++ src/main.py
@@ -1,18 +1,28 @@
 import data_processing as dp
+import model_training as mt 
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
-data=pd.read_csv('./data/heart.csv')
 
-X=data.drop(columns=['target'])
-Y=data['target']
-
-
+import matplotlib.pyplot as plt
+import numpy as np
+data = pd.read_csv('./data/heart.csv')
 
 dataprocessing = dp.Data_processing()
 data = dataprocessing.delete_duplicates(data)
+X = data.drop(columns=['target'])
+Y = data['target']
+
 x_train, x_test, y_train, y_test = dataprocessing.split_data(X, Y)
 x_train = dataprocessing.fit_transform(x_train)
 x_test  = dataprocessing.transform(x_test)
+
+
+modelraining= mt.model_training()
+modelraining.train_model_RandomForest(x_train, y_train)
+modelraining.evalute(x_test, y_test)
+print(metrics.f1_score(y_train, modelraining.predict(x_train)))
+print(modelraining.feature_importance_Forestrandom(x_train))
+
--- src/model_training.py
+++ src/model_training.py
@@ -1,4 +1,4 @@
-
+import pandas  as pd
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -11,18 +11,50 @@ class model_training:
         self.model = RandomForestClassifier(n_estimators=100, random_state=42 , max_depth=5
                                             )
         self.model.fit(x_train, y_train)
+        
+        
     def train_model_LogisticRegression(self, x_train, y_train):
         self.model = LogisticRegression()
         self.model.fit(x_train, y_train)  
+    #Detta är en gemensam metod för att göra förutsägelser med den tränade modellen.
+    # Den tar x_test som input och returnerar modellens förutsägelser genom att anropa predict-metoden på modellen. 
+    # Detta gör det möjligt att enkelt generera förutsägelser på testdata eller annan data
     def predict(self, x_test):
         return self.model.predict(x_test)
     
     def evaluate_model(self, x_test, y_test):
-        
-        if self.model is None:
-            raise ValueError("Model has not been trained yet.")
         pred = self.predict(x_test)
         print(metrics.classification_report(y_test, pred))
         print(metrics.confusion_matrix(y_test, pred))
+    
+    # Denna metod utvärderar den tränade modellen på träningsdata. Den gör 
+    # förutsägelser på x_train och jämför dem med y_train för att generera en klassificeringsrapport och en förvirringsmatris, 
+    # som sedan skrivs ut. Detta kan hjälpa till att bedöma modellens prestanda på träningsdata.
+     
+    def evalute_trained_model(self, x_train, y_train):
+      pred = self.predict(x_train)
+      print(metrics.classification_report(y_train, pred))
+      print(metrics.confusion_matrix(y_train, pred))
+    
+    def evalute(self, x_test, y_test):
+        pred=self.predict(x_test)
+        print("accuracy:"+str(metrics.accuracy_score(y_test, pred)))
+        print("precision:"+str(metrics.precision_score(y_test, pred)))
+        print("f1-score:"+str(metrics.f1_score(y_test, pred)))
+        print("recall:"+str(metrics.recall_score(y_test, pred)))
         
-   
+    def feature_importance_Forestrandom(self,X):
+           
+            importances = self.model.feature_importances_
+            feature_names = X.columns
+
+            print("len(feature_names):", len(feature_names))
+            print("len(importances):", len(importances))
+            print("feature names:", list(feature_names))
+
+            df = pd.DataFrame({
+                "feature": feature_names,
+                "importance": importances
+            }).sort_values(by="importance", ascending=False)
+
+            return df
